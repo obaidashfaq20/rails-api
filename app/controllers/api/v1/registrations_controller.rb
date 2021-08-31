b9a323af3c9136c84afde6096b8ea3960e441ddc
obaidashfaq20 <obaidashfaq20@yahoo.com>
2021-08-31 13:45:07 +0500

    Response clean-up

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -4,19 +4,9 @@ class Api::V1::RegistrationsController < Devise::RegistrationsController
   def create
     user = User.new user_params
     if user.save
-      render json: {
-        messages: "Signed Up Successfully",
-        is_success: true,
-        data: {
-          user: user
-        }
-      }, status: :ok
+      json_response "Signed up succesfully", true, {user: user}, :ok
     else
-      render json: {
-        messages: "Something wrong",
-        is_success: false,
-        data: {}
-      }, status: :unprocessable_entity
+      json_response "Something wrong", false, {}, :unprocessable_entity
     end
   end
 
@@ -28,10 +18,6 @@ class Api::V1::RegistrationsController < Devise::RegistrationsController
 
   def ensure_params_exist
     return if params[:user].present?
-    render json: {
-      messages: "Missing params",
-      is_success: false,
-      data: {}
-    }, status: :bad_request
+    json_response "Missing params", false, {}, :bad_request
   end
 end
