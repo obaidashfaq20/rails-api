3574111b9a632859a45559e4d03d45392a4a1f8f
obaidashfaq20 <obaidashfaq20@yahoo.com>
2021-08-31 13:38:40 +0500

    Sign up and get authentication token

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  namespace :api, defaults: {format: :json} do
+    namespace :v1 do
+      devise_scope :user do
+        post "sign_up", to: "registrations#create"
+      end
+    end
+  end
 end
