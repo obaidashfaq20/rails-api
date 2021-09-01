1c72e143c32df92272d7b20d9e42e8a1908389be
obaidashfaq20 <obaidashfaq20@yahoo.com>
2021-09-01 11:59:27 +0500

    Eager loading and couter culture to reduce number of database queries

--- app/serializers/book_serializer.rb
+++ app/serializers/book_serializer.rb
@@ -1,9 +1,9 @@
 class BookSerializer < ActiveModel::Serializer
-  attributes :id, :title, :author, :image
+  attributes :id, :title, :author, :image,
         # :average_rating_of_book, :content_rating_of_book, :recommend_rating_of_book,
-        # :total_reviews
+        :total_reviews
   has_many :reviews
-
+  #
   #
   # def average_rating_of_book
   #   object.reviews.count == 0 ? 0 : object.reviews.average(:average_rating).round(1)
@@ -16,8 +16,8 @@ class BookSerializer < ActiveModel::Serializer
   # def recommend_rating_of_book
   #   object.reviews.count == 0 ? 0 : object.reviews.average(:recommend_rating).round(1)
   # end
-  # 
-  # def total_reviews
-  #   object.reviews_count
-  # end
+
+  def total_reviews
+    object.reviews_count
+  end
 end
